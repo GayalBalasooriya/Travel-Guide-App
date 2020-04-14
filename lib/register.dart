1171cfcaed1dd5a6604e056ae7a58dc2a97f8cb3
tharindu97 <tharindukavishna@gmail.com>
2020-04-14 11:41:24 +0530

    google_authentication

---
+++ lib/register.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Register Page'),
+          ],
+        )
+      ),
+    );
+  }
+}
